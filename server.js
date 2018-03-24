7bb25a722d8d732f31418745d87f0bac02bc13d1
Abhilash SB <abhilashsb9@gmail.com>
2018-03-24 13:06:08 +0530

    Session issue fix

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ var methodOverride = require('method-override');
 var service = require('./services/service.js');
 var session = require('express-session');
 
+app.use(session({ secret: 'schoolofshanthi' }));
 app.use(express.static(__dirname + '/public')); // set the static files location /public/img will be /img for users                
 app.use(bodyParser.urlencoded({ 'extended': 'true' })); // parse application/x-www-form-urlencoded
 app.use(bodyParser.json({ limit: '5000mb' })); // parse application/json
