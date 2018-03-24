32c27cba8ec35290b3ab39a38898bd3a5ce832ec
Abhilash SB <abhilashsb9@gmail.com>
2018-03-24 12:05:40 +0530

    Change in getting port

--- server.js
+++ server.js
@@ -86,5 +86,5 @@ app.get('/settings',function(req,res){
     res.render('settings.html');
 })
 // listen (start app with node server.js) ======================================
-    app.listen(8080);
+    app.listen(process.env.PORT || 8080);
     console.log("App listening on port 8080");
