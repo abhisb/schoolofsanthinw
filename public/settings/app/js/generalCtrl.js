e0924ecbfb6ec25c13408bcd73427b0fb5f0cf06
GRPRINC <q-burst.prince@diamler.com>
2018-04-14 12:33:57 +0530

    commit for optimizations

--- public/settings/app/js/generalCtrl.js
+++ public/settings/app/js/generalCtrl.js
@@ -60,4 +60,16 @@
         }
         $scope.getSavedImages();
     }]);
+
+    window.settingsApp.directive('onErrorSrc', function() {
+        return {
+            link: function(scope, element, attrs) {
+              element.bind('error', function() {
+                if (attrs.src != attrs.onErrorSrc) {
+                  attrs.$set('src', attrs.onErrorSrc);
+                }
+              });
+            }
+        }
+    });
 })();
